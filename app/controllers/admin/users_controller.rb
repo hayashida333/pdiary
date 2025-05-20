02f6ce06c2caa34d15ddb9fdaa27cb5f15986c91
hayashida333 <kenyalintian@gmail.com>
2025-05-20 19:51:45 +0900

    ユーザー一覧画面の追加 (#30)
    * サイドバーの追加
    
    * サイドバーの完成
    
    * 表の追加
    
    * 表の微調整
    
    * ユーザー一覧画面の作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,19 @@
 module Admin
   class UsersController < ApplicationController
     def index
+      @users = User.all
+    end
+
+    def show
+      @user = User.find(params[:id])
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def edit
+      @user = User.find(params[:id])
     end
   end
 end
