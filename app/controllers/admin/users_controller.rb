1ed11ac5069bc498dc4c5da9b4d22f6769ad616b
hayashida333 <kenyalintian@gmail.com>
2025-05-28 19:32:55 +0900

    admin/usersの追加 (#32)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+#frozen_string_literal: true
 module Admin
   class UsersController < ApplicationController
     def index
@@ -13,8 +13,22 @@ module Admin
       @user = User.new
     end
 
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to admin_users_path
+      else
+        render :new
+      end
+    end
+
     def edit
       @user = User.find(params[:id])
+    end
+
+    private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :tel_number)
     end
   end
 end
