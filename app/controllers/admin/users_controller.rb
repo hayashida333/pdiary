1cf1fe110c8c7a634bc18925681c42a0dfcac636
hayashida <kenyalintian@gmail.com>
2025-06-07 14:04:22 +0900

    更新機能の実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,4 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 module Admin
   class UsersController < ApplicationController
     def index
@@ -26,6 +26,22 @@ module Admin
       @user = User.find(params[:id])
     end
 
+   def update
+  @user = User.find(params[:id])
+
+  if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+    params[:user].extract!(:password, :password_confirmation)
+  end
+
+  if @user.update(user_params)
+    redirect_to admin_user_path(@user), notice: 'ユーザー情報を更新しました'
+  else
+    # ここでエラーログを出す
+    Rails.logger.debug "更新失敗: #{@user.errors.full_messages.join(', ')}"
+    render :edit
+  end
+end
+
     def destroy
       @user = User.find(params[:id])
       @user.destroy
@@ -33,6 +49,7 @@ module Admin
     end
 
     private
+
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :tel_number)
     end
